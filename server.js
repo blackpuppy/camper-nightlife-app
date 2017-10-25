2c494ec9a2a456f6e847843f7233b2425bc0a181
Zhu Ming <mingzhu.z+github@gmail.com>
2017-10-25 12:17:44 +0800

    twitter authentication

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
+var express = require('express'),
+    routes = require('./app/routes/index.js'),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    session = require('express-session'),
+    bodyParser = require('body-parser'),
+    path = require('path');
 
 var app = express();
 require('dotenv').load();
@@ -13,14 +15,18 @@ require('./app/config/passport')(passport);
 mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public/js', express.static(process.cwd() + '/node_modules'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
 
 app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
+    secret: 'secretClementine',
+    resave: false,
+    saveUninitialized: true
 }));
 
 app.use(passport.initialize());
@@ -30,5 +36,5 @@ routes(app, passport);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+    console.log('Node.js listening on port ' + port + '...');
 });
