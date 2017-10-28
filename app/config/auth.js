88d7ac8a39c86504aed8e2fa92f602e06ce9675b
Zhu Ming <mingzhu.z+github@gmail.com>
2017-10-28 11:36:03 +0800

    twitter authentication

--- app/config/auth.js
+++ app/config/auth.js
@@ -1,9 +1,13 @@
 'use strict';
 
 module.exports = {
-	'twitterAuth': {
-		'consumerKey': process.env.TWITTER_KEY,
-		'consumerSecret': process.env.TWITTER_SECRET,
-		'callbackURL': process.env.APP_URL + 'auth/twitter/callback'
-	}
+    'twitterAuth': {
+        'consumerKey': process.env.TWITTER_CONSUMER_KEY,
+        'consumerSecret': process.env.TWITTER_CONSUMER_SECRET,
+        'callbackURL': process.env.APP_URL + 'auth/twitter/callback'
+    },
+    'yelpAuth': {
+        'clientID': process.env.YELP_CLIENT_ID,
+        'clientSecret': process.env.YELP_CLIENT_SECRET,
+    }
 };
